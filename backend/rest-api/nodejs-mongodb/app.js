2aadf5d8c4dc026d7149a9d9245da1acc943e851
Sachin Gupta <saching@microsoft.com>
2019-07-21 17:36:36 -0700

    adding error handling

--- backend/rest-api/nodejs-mongodb/app.js
+++ backend/rest-api/nodejs-mongodb/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const tasksRoutes = require('./api/routes/tasks');
 
+// setup logging middleware using morgan
+app.use(morgan('dev'));
+
 // To setup middleware
 app.use('/tasks', tasksRoutes);
+
+// Handle error
+app.use((req, res, next) => {
+    const error = new Error('API not supported');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
