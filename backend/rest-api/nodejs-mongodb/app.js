2ccf6b5c7fe418a48c1f0cb7c6efad40e2c55fc3
Sachin Gupta <saching@microsoft.com>
2019-07-21 17:54:55 -0700

    handling cors

--- backend/rest-api/nodejs-mongodb/app.js
+++ backend/rest-api/nodejs-mongodb/app.js
@@ -12,6 +12,17 @@ app.use(morgan('dev'));
 app.use(bodyeParser.urlencoded({extended: false}));
 app.use(bodyeParser.json());
 
+// Handling CORS
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', '*');
+   if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+        return res.status(200).json({});
+   }
+   next();
+});
+
 // To setup middleware
 app.use('/tasks', tasksRoutes);
 
