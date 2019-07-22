6d172eb44a275da719b10a9c0503178986e0d1a6
Sachin Gupta <saching@microsoft.com>
2019-07-22 11:52:42 -0700

    updating todo

---
+++ frontend/react/react-hooks/build/precache-manifest.fd71f7003ba432b9b21f93e01ae76d6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aaa52cd98710cdaa61421bb7088b60a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e55d6a9d296db33293f",
+    "url": "/static/css/main.96dcf6eb.chunk.css"
+  },
+  {
+    "revision": "4722ca8aeff4aa24a171",
+    "url": "/static/js/2.9406b900.chunk.js"
+  },
+  {
+    "revision": "7e55d6a9d296db33293f",
+    "url": "/static/js/main.4884aaf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
